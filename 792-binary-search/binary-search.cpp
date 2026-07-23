7c2aeded64d96850e94d9caabc0a5156816d0250
Arnendu Biswas <arnendubiswas5@gmail.com>
2026-07-23 02:38:28 -0700

    Time: 0 ms (100.00%) | Memory: 31.3 MB (46.82%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        
+      while (low<=high){
+        int mid=low+(high-low)/2;
+            if (nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+            
+        }
+        return -1;
+    }
+   
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
